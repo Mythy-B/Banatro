e83ab9d73356b7dc132410e19a923c079919a34a
Maximilian Bartecki <m.bartecki@prologistik.com>
2026-05-08 14:44:04 +0200

    Added Banana Card (Rank) WIP.

---
+++ items/ranks.lua
@@ -0,0 +1,10 @@
+SMODS.Rank {
+    key = "Banana",
+    card_key = "B",
+    pos = { x = 13},
+    nominal = 15,
+    face_nominal = 0.5,
+    shorthand = "B",
+    straight_edge = false,
+    strength_effect = {ignore = true},
+}
