110e39d590fe0dda61bcc843280b624a8c913965
Maximilian Bartecki <m.bartecki@prologistik.com>
2026-05-08 16:26:22 +0200

    Rahhh.

--- items/ranks.lua
+++ items/ranks.lua
@@ -1,10 +1,20 @@
+SMODS.Atlas {
+    key = "ranks",
+    path = "banana.png",
+    px = 71,
+    py = 95
+}
+
 SMODS.Rank {
     key = "Banana",
     card_key = "B",
-    pos = { x = 13},
+    loc_text = {
+        name = "Banana"
+    },
+    atlas = 0,
+    pos = { x = 0},
     nominal = 15,
     face_nominal = 0.5,
-    shorthand = "B",
-    straight_edge = false,
     strength_effect = {ignore = true},
+    suit_map = {Hearts = 0, Clubs = 0, Diamonds = 0, Spades = 0}
 }
